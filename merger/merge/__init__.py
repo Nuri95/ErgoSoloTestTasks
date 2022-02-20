b0fc477545c6e16e4da56e53c7805b2e658feadc
Nuri <nurhotun95@mail.ru>
2022-02-20 15:00:48 +0500

    объединяю в кортеж строки из файлов

--- merger/merge/__init__.py
+++ merger/merge/__init__.py
@@ -3,5 +3,30 @@ __all__ = (
 )
 
 
-def merge():
-    pass
+def merge(file_with_strings, file_with_numbers, count_lines_f1, count_lines_f2):
+    if count_lines_f1 >= count_lines_f2:
+        for string in file_with_strings:
+            line_with_number = None
+            for number in file_with_numbers:
+                if number != '\n':
+                    line_with_number = number.replace('\n', '')
+                break
+
+            if string == '\n':
+                continue
+
+            yield string.replace('\n', ''), line_with_number
+
+    else:
+        for line_with_number in file_with_numbers:
+            line_with_string = None
+            for string in file_with_strings:
+                if string != '\n':
+                    line_with_string = string.replace('\n', '')
+                break
+
+            if line_with_string is None or line_with_string == '\n':
+                continue
+
+            yield line_with_string, line_with_number
+
