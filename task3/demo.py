2a8855aa58ac0b6ae4b0c19d5fb27e9481a80f11
Nuri <nurhotun95@mail.ru>
2022-02-22 07:07:00 +0500

    опечатка

--- task3/demo.py
+++ task3/demo.py
@@ -3,4 +3,4 @@ from currency_rates import CbCurrencyRates
 currency_rates = CbCurrencyRates()
 rur_usd = currency_rates.get_currency_rate('USD')
 rur_eur = currency_rates.get_currency_rate('EUR')
-print(rur_eur, rur_eur)
+print(rur_usd, rur_eur)
