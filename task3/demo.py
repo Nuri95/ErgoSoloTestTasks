a989095b8776ecdc1064b4eed5663ab74c314466
Nuri <nurhotun95@mail.ru>
2022-02-22 12:26:09 +0500

    опечатку поправила

--- task3/demo.py
+++ task3/demo.py
@@ -5,7 +5,7 @@ cache = CurrencyCache()
 
 currency_rates = CbCurrencyRates(cache)
 
-rur_usd = currency_rates.get_currency_rate('EUR')
-rur_eur = currency_rates.get_currency_rate('USD')
-rur_usd2 = currency_rates.get_currency_rate('EUR')
+rur_usd = currency_rates.get_currency_rate('USD')
+rur_eur = currency_rates.get_currency_rate('EUR')
+rur_usd2 = currency_rates.get_currency_rate('USD')
 print(rur_usd, rur_eur, rur_usd2)
